91c932b2255c52fbc17c82c30058949d2e1770aa
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-25 00:35:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './componen
 
 const App = () => (
 <BrowserRouter>
-    <Box sx={{ backgroundColor: '#000' }}>
+<Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
         <Route exact path='/' element={<Feed />} />
