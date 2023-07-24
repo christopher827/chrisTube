80c52be406d40bafd41a380a7eca6d33d54604f2
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-25 00:35:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => (
 <BrowserRouter>
 <Box sx={{ backgroundColor: '#000' }}>
 <Navbar />
-      <Routes>
+<Routes>
         <Route exact path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
