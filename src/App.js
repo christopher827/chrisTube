9af6612928cdc1a421647a869ebe6b4d16c09a83
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-25 00:36:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => (
 <Routes>
 <Route exact path='/' element={<Feed />} />
 <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
+<Route path='/channel/:id' element={<ChannelDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
