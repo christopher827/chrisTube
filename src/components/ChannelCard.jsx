2c2a73fff2ca31364307fb4a0e85f00abf852a02
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-07 23:26:38 +0100

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -31,7 +31,7 @@ sx={{
         {channelDetail?.statistics?.subscriberCount && (
           <Typography sx={{ fontSize: '15px', fontWeight: 500, color: 'gray' }}>
             {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers
-      </Typography>
+</Typography>
 )}
 </CardContent>
 </Link>
