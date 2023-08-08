80e9f02f3b7a00d51d9118a9d46fc6ec903461e7
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-08 01:41:38 +0100

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -19,7 +19,7 @@ setChannelDetail(data?.items[0]);
 
 const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
 
-      setVideos(videosData?.items);
+setVideos(videosData?.items);
     };
 
     fetchResults();
