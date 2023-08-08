9e2866727a5cdf63da587c3451cc2f51c1d5c544
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-08 01:41:23 +0100

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -17,7 +17,7 @@ const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
 
 setChannelDetail(data?.items[0]);
 
-      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
 
       setVideos(videosData?.items);
     };
