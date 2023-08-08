1ebedd62aae3c74677a6da72ec5f87ccd80efb8e
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-08 01:39:49 +0100

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -25,7 +25,7 @@ sx={{
           sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
 />
 <Typography variant="h6">
-          {channelDetail?.snippet?.title}{' '}
+      {channelDetail?.snippet?.title}{' '}
 <CheckCircleIcon sx={{ fontSize: '14px', color: 'gray', ml: '5px' }} />
 </Typography>
 {channelDetail?.statistics?.subscriberCount && (
