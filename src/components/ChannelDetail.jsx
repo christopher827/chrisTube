b7fa569d2690ca663d20603fbd84c06fb9a07779
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-08 01:44:32 +0100

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -29,7 +29,7 @@ fetchResults();
   <Box minHeight="95vh">
   <Box>
   <div style={{
-        height:'300px',
+      height:'300px',
           background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
           zIndex: 10,
         }} />
