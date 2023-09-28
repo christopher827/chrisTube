ba61063d0fc0dd19f11af4d97eb18c8153927036
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-09-28 23:59:04 +0100

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -35,7 +35,7 @@ fetchResults();
   }} />
   <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
   </Box>
-      <Box p={2} display="flex">
+    <Box p={2} display="flex">
       <Box sx={{ mr: { sm: '100px' } }}/>
       <Videos videos={videos} />
     </Box>
